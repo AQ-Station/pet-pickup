592acd179d2beb1bb1d91b34173e10209d1ee20c
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2021-10-29 17:22:43 -1000

    basic pet-ready w/ added confirm modal

--- app/imports/ui/pages/PetReadyMessage.jsx
+++ app/imports/ui/pages/PetReadyMessage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
+import ConfirmCheckIn from '../components/ConfirmCheckIn';
 
 /** A simple static component to render some text for the landing page. */
 class PetReadyMessage extends React.Component {
   render() {
     return (
-      <Grid centered container verticalAlign='middle' textAlign='center' columns={1}>
+      <Grid container centered verticalAlign='middle' textAlign='center' columns={1} row={3} className='blue-background-body'>
+        <Grid.Column textAlign='center'>
+          <Icon inverted color='green' name='check circle' size='massive'/>
+          <Header as='h1' inverted className='center-text'>Your pet is ready!</Header>
+          <div className='confirm-message'>
+            <ConfirmCheckIn/>
+          </div>
 
-        <Grid.Row centered className='blue-background-body'>
-          <Header as="h1" inverted>Your pet is ready!</Header>
-        </Grid.Row>
+        </Grid.Column>
 
       </Grid>
     );
