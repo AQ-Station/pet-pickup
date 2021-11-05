f9a6c645746b569d8927b666dbc4436164fe5b75
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2021-11-05 03:20:02 -1000

    testing out sidebar/navbar changes

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,47 +3,79 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header, Image } from 'semantic-ui-react';
+import { Grid, Menu, Dropdown, Header, Image, Sidebar, Button, Icon } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
-/** The NavBar appears at the top of every page. Rendered by the App Layout component. */
+/** The NavBar appears at the top of every page. Rendered by the App Layout component.
+ * References: https://bit.dev/semantic-org/semantic-ui-react/sidebar?example=5e9c2ef1c772c5001968ba1f */
 class NavBar extends React.Component {
+  state = { visible: false }
+
+  handleShowOnClick = () => this.setState({ visible: true })
+
+  handleHideOnClick = () => this.setState({ visible: false })
+
+  handleSidebarHide = () => this.setState({ visible: false })
+
   render() {
-    const menuStyle = { marginBottom: '10px', backgroundColor: '#03669e' };
+    const { visible } = this.state;
+    const menuStyle = { paddingBottom: '-10px', backgroundColor: '#03669e' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
-        <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'><Image src='/images/aq-logo-nav.png' size="huge"/>AQ-Station</Header>
-        </Menu.Item>
-        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/PetRoster" key='pr'>Pet Roster</Menu.Item>
-        ) : ''}
-        {this.props.currentUser ? (
-          [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/addpet" key='addpet'>Add Pets</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/user" key='user'>User Page</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-        ) : ''}
-        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-        ) : ''}
-        <Menu.Item position="right">
-          {this.props.currentUser === '' ? (
-            <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          ) : (
-            <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </Menu.Item>
-      </Menu>
+      <Grid style={menuStyle}>
+        <Grid.Column>
+          <Menu style={menuStyle} attached="top" inverted>
+            <Button compact icon style={menuStyle}
+              onClick={this.handleShowOnClick}>
+              <Icon name='sign-in' size="large"/>
+            </Button>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/">
+              <Header inverted as='h1'><Image src='/images/aq-logo-nav.png' size="huge"/>AQ-Station</Header>
+            </Menu.Item>
+          </Menu>
+        </Grid.Column>
+        <Grid.Column>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            dimmed={visible}
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/PetRoster" key='pr'>Pet Roster</Menu.Item>
+            ) : ''}
+            {this.props.currentUser ? (
+              [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/addpet" key='addpet'>Add Pets</Menu.Item>,
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/user" key='user'>User Page</Menu.Item>,
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
+            ) : ''}
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+            ) : ''}
+            <Menu.Item position="right">
+              {this.props.currentUser === '' ? (
+                <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
+                    <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : (
+                <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
+                  <Dropdown.Menu>
+                    <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+                  </Dropdown.Menu>
+                </Dropdown>
+              )}
+            </Menu.Item>
+          </Sidebar>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
